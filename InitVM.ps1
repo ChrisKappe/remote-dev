6221096c8bc8dab4b9f7212d4a2b6f88ec60b65d
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-05-01 00:22:19 +0200

    might work

---
+++ InitVM.ps1
@@ -0,0 +1,17 @@
+param (
+    [Parameter()]
+    [string]
+    $sshKey
+)
+
+# install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+choco install git
+choco install vim
+choco install openssh -params '"/SSHServerFeature"'
+
+# configure SSH
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cosmoconsult/remotedev/$branch/sshd_config" -UseBasicParsing -OutFile "c:\ProgramData\ssh\sshd_config"
+restart-service sshd
+$sshKey | Out-File 'c:\programdata\ssh\administrators_authorized_keys'
