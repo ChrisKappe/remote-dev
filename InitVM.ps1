8000c0869d95e15c1c010559df5497919df2c659
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-05-01 22:33:56 +0200

    fix encoding and access rights

--- InitVM.ps1
+++ InitVM.ps1
@@ -17,5 +17,16 @@ choco install openssh -params '"/SSHServerFeature"'
 
 # configure SSH
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cosmoconsult/remote-dev/$branch/sshd_config" -UseBasicParsing -OutFile "c:\ProgramData\ssh\sshd_config"
+$sshKey | Out-File 'c:\programdata\ssh\administrators_authorized_keys' -Encoding utf8
+### adapted (pretty much copied) from https://gitlab.com/DarwinJS/ChocoPackages/-/blob/master/openssh/tools/chocolateyinstall.ps1#L433
+$path = "c:\ProgramData\ssh\administrators_authorized_keys"
+$acl = Get-Acl -Path $path
+# following SDDL implies 
+# - owner - built in Administrators
+# - disabled inheritance
+# - Full access to System
+# - Full access to built in Administrators
+$acl.SetSecurityDescriptorSddlForm("O:BAD:PAI(A;OICI;FA;;;SY)(A;OICI;FA;;;BA)")
+Set-Acl -Path $path -AclObject $acl
+### end of copy
 restart-service sshd
-$sshKey | Out-File 'c:\programdata\ssh\administrators_authorized_keys'
